7d9745aed33c58056df595026bceed7283cc3671
Pranjal Jain <pranjalworkon@gmail.com>
2025-09-09 18:21:47 +0530

    feat: added auth system

---
+++ src/lib/auth.ts
@@ -0,0 +1,29 @@
+// server instance of better-auth
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { db } from '@/db'
+import { FullSchema } from '@/db/schema'
+
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: 'pg',
+        schema: FullSchema,
+    }),
+
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            prompt: 'select_account',
+        },
+    },
+
+    emailAndPassword: {
+        enabled: true,
+        requireEmailVerification: true, // require email verification before allowing sign in
+    },
+})
